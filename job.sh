26acadbda0f9aa03d007fa20d51ea5f76e63b131
Sergey Lukjanov <me@slukjanov.name>
2017-04-25 00:18:51 -0700

    Delete spark job before downloading

--- job.sh
+++ job.sh
@@ -3,4 +3,4 @@ set -ex
 
 spark_master=$(kubectl -n demo get po | awk '/spark-mas/{print $1}')
 
-echo "wget https://raw.githubusercontent.com/Frostman/tweetics/boston-demo/spark_hashtags_count.py && spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.1.0 spark_hashtags_count.py local[4] zk-kafka-1-0.zk-kafka-1:2181,zk-kafka-1-1.zk-kafka-1:2181,zk-kafka-1-2.zk-kafka-1:2181 twitter-stream 0 5 hdfs://hdfs-namenode:8020/demo" | kubectl -n demo exec "${spark_master}" -i bash
+echo "rm spark_hashtags_count.py; wget https://raw.githubusercontent.com/Frostman/tweetics/boston-demo/spark_hashtags_count.py && spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.1.0 spark_hashtags_count.py local[4] zk-kafka-1-0.zk-kafka-1:2181,zk-kafka-1-1.zk-kafka-1:2181,zk-kafka-1-2.zk-kafka-1:2181 twitter-stream 0 5 hdfs://hdfs-namenode:8020/demo" | kubectl -n demo exec "${spark_master}" -i bash
